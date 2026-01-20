aa6f3f8ff1986bc3ab123112c2507a9f0ddc6ef5
shreya5-eng <shreyapshetty.5@gmail.com>
2026-01-20 15:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from groq import Groq
 
 st.set_page_config("ShreyaAI Content Generator", layout="wide")
 st.title("📢 ShreyaAI – Content Generator")
-st.image("img.jpeg", use_container_width=60)
+st.image("img.jpeg", use_container_width=10)
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
